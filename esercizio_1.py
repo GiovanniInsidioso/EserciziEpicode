3a868f5fdcc31056b59644c3f7404b3347815866
GiovanniInsidioso <135722070+GiovanniInsidioso@users.noreply.github.com>
2023-09-05 20:46:39 +0200

    Primo Esercizio Python

---
+++ esercizio_1.py
@@ -0,0 +1,35 @@
+
+# abbiamo 25 studenti; memorizzare questo dato in una variabile
+
+studenti = 25
+
+#stampare a video
+
+print(studenti)
+
+#arrivano altri 3 studenti, memorizzare questo dato in un'altra variabile
+
+studenti = 25
+altri_studenti =3 
+
+#crea una terza variabile con la somma delle due precedenti
+
+studenti = 25
+altri_studenti =3 
+somma_studenti= studenti + altri_studenti
+print(somma_studenti)
+
+#crare a meno una lista 
+
+lista_numeri = [0, 1 , 3 , 4, 5]
+print("la lista è questa:", lista_numeri)
+
+lista_range = range(6)
+print(list(lista_range))
+
+#creare lista film preferiti
+
+lista_film = ("il signore degli anelli", 
+              "star wars", 
+              "il gladiatore")
+print(lista_film)
